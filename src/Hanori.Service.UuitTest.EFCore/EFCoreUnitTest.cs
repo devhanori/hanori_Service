124304746bfa884748eda0f28b96e5d71c2e4978
leehanol <dev.hanori@gmail.com>
2023-07-19 07:59:21 +0900

    EFCoreUnitTest TEST

---
+++ src/Hanori.Service.UuitTest.EFCore/EFCoreUnitTest.cs
@@ -0,0 +1,48 @@
+using Hanori.Service.EFCore;
+using System.Diagnostics;
+
+namespace Hanori.Service.UuitTest.EFCore
+{
+    [TestClass]
+    public class EFCoreUnitTest : EFCoreManager<Sample>
+    {
+        [TestMethod]
+        public void TestSyncAsync()
+        {
+            //TestMethod();
+
+            //TestMethodAsync().Wait();
+        }
+
+        public void TestMethod()
+        {
+            for(int i = 0; i < 5000; i++)
+            {
+                TestCRUD();
+            }
+        }
+
+        
+        public async Task TestMethodAsync()
+        {
+            for (int i = 0; i < 5000; i++)
+            {
+                await TestCRUDAsync();
+            }
+        }
+        
+        public void TestCRUD()
+        {
+            var entity = new Sample();
+
+            CreateOne(entity);
+        }
+
+        public async Task TestCRUDAsync()
+        {
+            var entity = new Sample();
+
+            await CreateOneAsync(entity);
+        }
+    }
+}
